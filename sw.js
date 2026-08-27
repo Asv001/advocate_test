14700a6b440381d38837a9b73aba5a7f1e2de476
Asv001 <a.1.svistunov@gmail.com>
2026-08-27 15:33:32 +0300

    Create sw.js
    service worker

---
+++ sw.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'advokat-exam-cache-v1';
+
+// Список файлов, которые нужно сохранить на телефон при первой загрузке
+const urlsToCache = [
+  './',
+  './index.html',
+  './questionnaire.yaml',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/js-yaml/4.1.0/js-yaml.min.js'
+];
+
+// Установка Service Worker и кэширование файлов
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Активация и очистка старых кэшей (если вы поменяете v1 на v2 в CACHE_NAME)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Стратегия: Stale-While-Revalidate (Сначала кэш, потом обновление в фоне)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      const fetchPromise = fetch(event.request).then(networkResponse => {
+        // Если скачалось успешно, обновляем кэш
+        if (networkResponse && networkResponse.status === 200) {
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return networkResponse;
+      }).catch(() => {
+        // Если нет сети, игнорируем ошибку fetch (приложение будет работать из кэша)
+      });
+
+      // Возвращаем из кэша моментально, если есть, иначе ждем загрузки из сети
+      return cachedResponse || fetchPromise;
+    })
+  );
+});
